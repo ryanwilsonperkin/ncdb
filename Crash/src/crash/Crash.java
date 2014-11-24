072d4ccd9bb2c686d85220ada266323f50847e43
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-11-24 12:43:05 -0500

    Parse number of threads. Prints query numbers.

--- Crash/src/crash/Crash.java
+++ Crash/src/crash/Crash.java
@@ -22,13 +22,36 @@ public class Crash {
             System.exit(1);
         }
         List<Record> records;
-        
         String filename = args[0];
+        int n_threads = Integer.parseInt(args[1]);
+        
         try {
             records = Record.loadFile(filename);
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
+        }
+        
+        for (int i = 2; i < args.length; i++) {
+            switch(args[i]) {
+                case "1":
+                    System.out.println("$Q1");
+                    break;
+                case "2":
+                    System.out.println("$Q2");
+                    break;
+                case "3":
+                    System.out.println("$Q3");
+                    break;
+                case "4":
+                    System.out.println("$Q4");
+                    break;
+                case "5":
+                    System.out.println("$Q5");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
